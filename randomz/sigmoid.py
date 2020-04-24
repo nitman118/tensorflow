a0e10754dbbdc33455a5d9239306be4939090c4f
Nitish Singh <nitman118@gmail.com>
2020-04-24 22:59:45 +0200

    random

--- randomz/sigmoid.py
+++ randomz/sigmoid.py
@@ -8,8 +8,3 @@ print(tf.config.experimental.list_physical_devices("GPU"))
 print("Is the Tensor on GPU #0:  "),
 print(x.device.endswith('GPU:0'))
 
-@tf.function
-def sig(x):
-    return 1/(1-tf.exp(-x))
-
-print(sig(3.0))
